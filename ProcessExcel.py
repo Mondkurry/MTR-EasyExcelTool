9a6449a4cb8354d66ed463b0e2781066cba83c4e
Aryan Mondkar <aryan.mondkar@gmail.com>
2023-06-20 12:52:24 -0700

    Update Reformate Sheet code

--- ProcessExcel.py
+++ ProcessExcel.py
@@ -1,5 +1,7 @@
 import openpyxl
-from openpyxl.utils import get_column_letter
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.styles import PatternFill, Alignment
 import numpy as np
 
 def populate_array_from_excel(file_path, sheet_name):
@@ -61,3 +63,63 @@ def process_array(file_path, sheet_name, columns_to_check):
     ProcessedState_UniqueIndices, ProcessedState_Sheet = get_unique_rows(DefaultState_Sheet, columns_to_check)
     
     return ProcessedState_Sheet, ProcessedState_UniqueIndices
+
+def update_input_sheet(file_path, sheet_name, array):
+    wb = openpyxl.load_workbook(file_path)
+
+    if sheet_name in wb.sheetnames:
+        sheet = wb[sheet_name]
+    else:
+        sheet = wb.create_sheet(title=sheet_name)
+
+    # Clear previous data in the sheet
+    sheet.delete_rows(1, sheet.max_row)
+
+    # Add the array values to the sheet
+    for row in array:
+        sheet.append(row)
+
+    reformat_excel_sheet(file_path, sheet_name)
+    # Save the workbook
+    wb.save(file_path)
+
+
+def reformat_excel_sheet(file_path, sheet_name):
+    # Load the workbook
+    workbook = load_workbook(file_path)
+    sheet2 = workbook[sheet_name]
+
+    # Apply light blue color to the first row
+    light_blue_fill = PatternFill(start_color='B7C9E3', end_color='B7C9E3', fill_type='solid')
+    for cell in sheet2[1]:
+        cell.fill = light_blue_fill
+
+    # Center-align all cells
+    for row in sheet2.iter_rows():
+        for cell in row:
+            cell.alignment = Alignment(horizontal='center')
+
+    # Auto-fit column width to contain text
+    for column in sheet2.columns:
+        max_length = 0
+        for cell in column:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(cell.value)
+            except TypeError:
+                pass
+        adjusted_width = (max_length + 2) * 1.2  # Add some padding
+        sheet2.column_dimensions[column[0].column_letter].width = adjusted_width
+
+    # Save the modified workbook
+    workbook.save('Input Sheet.xlsx')
+
+
+def process_and_update_userinput_sheet(file_path, default_sheet_name, userinput_sheet_name, columns_to_check):
+    ProcessedState_InputSheet, ProcessedState_UniqueIndices = process_array(
+        file_path, default_sheet_name, columns_to_check)
+    
+    update_input_sheet(file_path, userinput_sheet_name,
+                       ProcessedState_InputSheet)
+
+
--- main.py
+++ main.py
@@ -2,81 +2,25 @@
 
 import openpyxl
 from openpyxl.styles import PatternFill, Alignment
-from ProcessExcel import process_array
+from ProcessExcel import process_and_update_userinput_sheet
 import numpy as np
 from array import *
 
+file_path = "workbook1.xlsx"
+default_sheet_name = "Sheet1"
+input_sheet_name = "Input Sheet"
+# Specify the columns to concatenate (e.g., A=1, B=2, D=4)
+input_columns_to_check = [2, 4, 5, 7]
 
 
 def print_array(input_array):
-    for row in input_array:
-        print(row)
-
-
-def reformat_sheet(file_path, sheet_name):
-    wb = openpyxl.load_workbook(file_path)
-    sheet = wb[sheet_name]
-
-    # Set light blue color to the first row
-    first_row = sheet[1]
-    for cell in first_row:
-        cell.fill = PatternFill(start_color="BFEFFF", end_color="BFEFFF", fill_type="solid")
-
-    # Center all text in cells
-    for row in sheet.iter_rows(min_row=1):
-        for cell in row:
-            cell.alignment = Alignment(horizontal='center', vertical='center')
-
-    # Adjust column widths to fit cell contents
-    for column in sheet.columns:
-        max_length = 0
-        column_letter = column[0].column_letter
-        for cell in column:
-            try:
-                if len(str(cell.value)) > max_length:
-                    max_length = len(str(cell.value))
-            except TypeError:
-                pass
-        adjusted_width = (max_length + 2) * 1.2
-        sheet.column_dimensions[column_letter].width = adjusted_width
-
-    # Save the workbook
-    wb.save(file_path)
-
-
-def update_input_sheet(file_path, sheet_name, array):
-    wb = openpyxl.load_workbook(file_path)
-
-    if sheet_name in wb.sheetnames:
-        sheet = wb[sheet_name]
-    else:
-        wb.create_sheet(title=sheet_name)
-        sheet = wb[sheet_name]
-        
-    # Clear previous data in the sheet
-    sheet.delete_rows(1, sheet.max_row)
-
-    # Add the array values to the sheet
-    for row in array:
-        sheet.append(row)
-    
-    reformat_sheet(file_path, sheet_name)
-    # Save the workbook
-    wb.save(file_path)
-    
-
+  	for row in input_array:
+    		print(row)
+		
 def main():
-    input_file_path = "workbook1.xlsx"
-    default_sheet_name = "Sheet1"
-    input_sheet_name = "Input Sheet"
+	process_and_update_userinput_sheet(file_path, default_sheet_name, input_sheet_name, input_columns_to_check)
 
-    input_columns_to_check = [2, 4, 5, 7]  # Specify the columns to concatenate (e.g., A=1, B=2, D=4)
-
-    ProcessedState_InputSheet, ProcessedState_UniqueIndices = process_array(input_file_path, default_sheet_name, input_columns_to_check)
-    update_input_sheet(input_file_path, input_sheet_name, ProcessedState_InputSheet)
 
 
 if __name__ == "__main__":
     main()
-
-
