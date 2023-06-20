0929259ec3d159a46b5997ef8a40507758f26452
Aryan Mondkar <aryan.mondkar@gmail.com>
2023-06-19 20:07:42 -0700

    Working Backend for generating Input Sheet

--- ProcessExcel.py
+++ ProcessExcel.py
@@ -1,5 +1,6 @@
 import openpyxl
 from openpyxl.utils import get_column_letter
+import numpy as np
 
 def populate_array_from_excel(file_path, sheet_name):
     # Load the workbook
@@ -16,30 +17,47 @@ def populate_array_from_excel(file_path, sheet_name):
     return result_array
 
 
-def extract_columns(input_array, column_indices):
+def extract_columns(input_array, check_columns):
     output_array = []
     
     for row in input_array:
-        output_row = [row[index] for index in column_indices]
+        output_row = [row[index+1] for index in check_columns]
         output_array.append(output_row)
-    
+
     return output_array
 
+def compare_arrays(array1, array2): # Compares two arrays and returns the values for indices in array 1 that are present in array2
+    indexes = []
+    for value in array2:
+        if value in array1:
+            index = array1.index(value)
+            indexes.append(index)
+    return indexes
 
-def get_unique_rows(input_array):
+
+def get_unique_rows(input_array, check_columns):
+
+    Array_OnlyCheckColumns = extract_columns(input_array, check_columns)
+
+    unique_rows_onlyCheckColumns = []
     unique_rows = []
+    index_of_unique = []
+    index = 0
 
-    for row in input_array:
-        if row not in unique_rows:
-            unique_rows.append(row)
+    for row in Array_OnlyCheckColumns:
+        if row not in unique_rows_onlyCheckColumns:
+            # index_of_unique.append(index)
+            unique_rows_onlyCheckColumns.append(row)
+            unique_rows.append(input_array[index])
+        index += 1
 
-    return unique_rows
+    index_of_unique = compare_arrays(unique_rows_onlyCheckColumns, Array_OnlyCheckColumns)
+    
+    return index_of_unique, unique_rows
 
 
 def process_array(file_path, sheet_name, columns_to_check):
     DefaultState_Sheet = populate_array_from_excel(file_path, sheet_name)
-
-    ProcessedState_Sheet = extract_columns(DefaultState_Sheet, columns_to_check)
-    ProcessedState_Sheet = get_unique_rows(ProcessedState_Sheet)
-
-    return ProcessedState_Sheet
+    ProcessedState_UniqueIndices, ProcessedState_Sheet = get_unique_rows(DefaultState_Sheet, columns_to_check)
+    
+    return ProcessedState_Sheet, ProcessedState_UniqueIndices
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+# -*- coding: utf-8 -*-
+
 import openpyxl
 from openpyxl.utils import get_column_letter
 from ProcessExcel import process_array
+import numpy as np
+from array import *
+
 
 
 def print_array(input_array):
@@ -8,15 +13,37 @@ def print_array(input_array):
         print(row)
 
 
+def update_input_sheet(file_path, sheet_name, array):
+    wb = openpyxl.load_workbook(file_path)
+
+    if sheet_name in wb.sheetnames:
+        sheet = wb[sheet_name]
+    else:
+        sheet = wb.create_sheet(title=sheet_name)
+        
+    # Clear previous data in the sheet
+    sheet.delete_rows(1, sheet.max_row)
+
+    # Add the array values to the sheet
+    for row in array:
+        sheet.append(row)
+
+    # Save the workbook
+    wb.save(file_path)
+    
+
 def main():
     input_file_path = "workbook1.xlsx"
-    input_sheet_name = "Sheet1"
-    input_columns_to_check = [3, 4, 5, 6]  # Specify the columns to concatenate (e.g., A=1, B=2, D=4)
+    default_sheet_name = "Sheet1"
+    input_sheet_name = "Input Sheet"
 
-    ProcessedState_InputSheet = process_array(input_file_path, input_sheet_name, input_columns_to_check)
+    input_columns_to_check = [2, 4, 5, 7]  # Specify the columns to concatenate (e.g., A=1, B=2, D=4)
 
-    print_array(ProcessedState_InputSheet)
+    ProcessedState_InputSheet, ProcessedState_UniqueIndices = process_array(input_file_path, default_sheet_name, input_columns_to_check)
+    update_input_sheet(input_file_path, input_sheet_name, ProcessedState_InputSheet)
 
 
 if __name__ == "__main__":
     main()
+
+
