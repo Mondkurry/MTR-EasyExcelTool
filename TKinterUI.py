3125ea8dd554de104432aff66ee959e9be4bfe05
Aryan Mondkar <aryan.mondkar@gmail.com>
2023-06-25 22:20:55 -0700

    Trying to get File name working does not work rn

--- TKinterUI.py
+++ TKinterUI.py
@@ -4,7 +4,7 @@
 import tkinter as tk
 import customtkinter
 from tkinter import *
-from tkinter import filedialog
+from tkinter import filedialog as fd
 import numpy as np
 import sys
 
@@ -15,8 +15,11 @@ containerColor = '#4A5859'       # Light Grey
 textColor = '#F5C396'           # Light Orange
 
 class MTR_EasyExcel_Tool(tk.Tk):
+
+    
     def __init__(self):
         super().__init__()
+        self.file_path = ""
         self.title("MTR EasyExcel Tool")
         self.geometry("800x800")
         #self.resizable(False, False)
@@ -25,6 +28,8 @@ class MTR_EasyExcel_Tool(tk.Tk):
         self.grid_columnconfigure((0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16), weight=0)
         self.grid_rowconfigure((0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16), weight=0)
         self.grid_columnconfigure(0, weight=1, uniform="fred")
+
+        ################### CREATE BOUNDING BOXES FOR ALL APP ICONS ###################
 
         self.Title = customtkinter.CTkLabel(self, text="MTR EasyExcel Tool", 
                                             font=("Arial", 36, "bold"), 
@@ -35,7 +40,7 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                             width=600-20,
                                             padx=10,
                                             pady=10,)
-        self.Title.grid(row=0, column=5, pady=10, padx=10, columnspan=12, rowspan=4)
+        self.Title.grid(row=0, column=5, pady=10, padx=10, rowspan=4, columnspan=12)
 
         self.importButtonInstructions = customtkinter.CTkLabel(self, 
                                                                text="Import File:", 
@@ -74,7 +79,7 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                         padx=10,
                                                         pady=10,
                                                         anchor="n")
-        self.File_Preview.grid(row=12, column=0,  pady=10, padx=10, columnspan=16, rowspan=4)
+        self.File_Preview.grid(row=12, column=0,  pady=10, padx=10,  rowspan=4, columnspan=16)
 
         self.File_Preview = customtkinter.CTkLabel(self,
                                                         text="Populate Fields Buttons",
@@ -87,7 +92,61 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                         padx=10,
                                                         pady=10,
                                                         anchor="n")
-        self.File_Preview.grid(row=16, column=0,  pady=10, padx=10, columnspan=16, rowspan=4)
+        self.File_Preview.grid(row=16, column=0,  pady=10, padx=10, rowspan=4, columnspan=16)
+
+        ################### IMPORT BUTTON STUFF ###################
+
+        # Create the button that will open the file dialog
+        self.import_button = customtkinter.CTkButton(self, 
+                                    height = 30,
+                                    text="Import",
+                                    font=("Arial", 16, "bold"), 
+                                    corner_radius=5,
+                                    fg_color = textColor,
+                                    text_color=backgroundColor,
+                                    hover = DISABLED,
+                                    command=self.open_file
+                                    )
+        self.import_button.grid(row=3, column=0,  pady=10, padx=10)
+
+        self.CurrentFileText = customtkinter.CTkLabel(self,
+                                                    text="Current File: ",
+                                                    width = 150,
+                                                    font=("Arial", 12, "bold"),
+                                                    fg_color=containerColor,
+                                                    text_color=textColor,
+                                                    anchor="nw")
+        self.CurrentFileText.grid(row=4, column=0, pady=10, padx=10)
+
+        self.currentFile = customtkinter.CTkLabel(self,
+                                                    text=self.readable_file_path,
+                                                    width = 150,
+                                                    font=("Arial", 16, "bold"),
+                                                    fg_color=containerColor,
+                                                    text_color=textColor,
+                                                    anchor="nw")
+        self.currentFile.grid(row=5, column=0, pady=10, padx=10)
+
+        # Create the open_file function that will open the file dialog
+      
+
+        # Create the open_file function that will open the file dialog
+
+    def set_file_path(self, file_path):
+        self.file_path = file_path
+    
+    def get_file_path(self):
+        return self.file_path
+    
+    def readable_file_path(self):
+        return self.file_path.split(".xlsx")[-1]
+
+    def open_file(self):
+        file = fd.askopenfilename(filetypes=[("Excel files", "*.xlsx")])
+        self.set_file_path(file)
+
+
+
 
 
 def main():
