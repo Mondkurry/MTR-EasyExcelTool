efddd192480a167d9c60a99eec913f4e2ad1f023
Aryan Mondkar <aryan.mondkar@gmail.com>
2023-06-25 17:36:56 -0700

    fixed scaling issues

--- TKinterUI.py
+++ TKinterUI.py
@@ -19,28 +19,23 @@ class MTR_EasyExcel_Tool(tk.Tk):
         super().__init__()
         self.title("MTR EasyExcel Tool")
         self.geometry("800x800")
-        self.resizable(False, False)
+        #self.resizable(False, False)
         customtkinter.set_appearance_mode("dark")
-        self.grid_columnconfigure((0,1,2,4), weight=1)
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_rowconfigure(1, weight=2)
-        self.grid_rowconfigure(2, weight=2)
-        self.grid_rowconfigure(3, weight=3)
-        self.grid_rowconfigure(4, weight=2)
+
+        self.grid_columnconfigure((0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16), weight=0)
+        self.grid_rowconfigure((0,1,2,4,5,6,7,8,9,10,11,12,13,14,15,16), weight=0)
         self.grid_columnconfigure(0, weight=1, uniform="fred")
 
         self.Title = customtkinter.CTkLabel(self, text="MTR EasyExcel Tool", 
-                                            font=("Arial", 28, "bold"), 
+                                            font=("Arial", 36, "bold"), 
                                             fg_color=containerColor,
                                             text_color=textColor,
                                             corner_radius=10,
-                                            height=((1/8) *800),
-                                            width=(3/4)*800,
+                                            height=100-20,
+                                            width=600-20,
                                             padx=10,
                                             pady=10,)
-        self.Title.grid(row=0, column=1, pady=10, padx=10, rowspan=1, columnspan=3)
-        
-
+        self.Title.grid(row=0, column=5, pady=10, padx=10, columnspan=12, rowspan=4)
 
         self.importButtonInstructions = customtkinter.CTkLabel(self, 
                                                                text="Import File:", 
@@ -48,12 +43,12 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                                corner_radius=10, 
                                                                fg_color=containerColor,
                                                                text_color=textColor,
-                                                               height=(3/8)*800,
-                                                               width = 1/4*800,
+                                                               height=250-20,
+                                                               width = 200-20,
                                                                padx = 10,
                                                                pady = 10,
                                                                anchor="n")
-        self.importButtonInstructions.grid(row=0, column=0,  pady=10, padx=10, rowspan=3, columnspan=1)
+        self.importButtonInstructions.grid(row=0, column=0,  pady=10, padx=10, rowspan=12, columnspan=4)
 
         self.Pick_col_section = customtkinter.CTkLabel(self,
                                                         text="Pick Columns",
@@ -61,12 +56,12 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                         corner_radius=10,
                                                         fg_color=containerColor,
                                                         text_color=textColor,
-                                                        height = (1/4)*800,
-                                                        width = (3/4)*800,
+                                                        height = 150-20,
+                                                        width = 600-20,
                                                         padx=10,
                                                         pady=10,
                                                         anchor="n")
-        self.Pick_col_section.grid(row=1, column=1,  pady=10, padx=10, rowspan=2, columnspan=3)
+        self.Pick_col_section.grid(row=4, column=4,  pady=10, padx=10, rowspan=8, columnspan=12)
 
         self.File_Preview = customtkinter.CTkLabel(self,
                                                         text="File Preview",
@@ -74,12 +69,12 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                         corner_radius=10,
                                                         fg_color=containerColor,
                                                         text_color=textColor,
-                                                        height = 6/16*800,
-                                                        width = 800,
+                                                        height = 350-20,
+                                                        width = 800-20,
                                                         padx=10,
                                                         pady=10,
                                                         anchor="n")
-        self.File_Preview.grid(row=3, column=0,  pady=10, padx=10, columnspan=4)
+        self.File_Preview.grid(row=12, column=0,  pady=10, padx=10, columnspan=16, rowspan=4)
 
         self.File_Preview = customtkinter.CTkLabel(self,
                                                         text="Populate Fields Buttons",
@@ -87,31 +82,12 @@ class MTR_EasyExcel_Tool(tk.Tk):
                                                         corner_radius=10,
                                                         fg_color=containerColor,
                                                         text_color=textColor,
-                                                        height = 1/4*800,
-                                                        width = 800,
+                                                        height = 200-20,
+                                                        width = 800-20,
                                                         padx=10,
                                                         pady=10,
                                                         anchor="n")
-        self.File_Preview.grid(row=4, column=0,  pady=10, padx=10, columnspan=4)
-        
-        # # code to import a file using a button in tkinter labeled "import" the name of the current imported file should be printed right next to the button
-        # self.import_button = customtkinter.CTkButton(self, 
-        #                             width = 80,
-        #                             height = 30,
-        #                             text="Import",
-        #                             font=("Arial", 16, "bold"), 
-        #                             corner_radius=5,
-        #                             fg_color = textColor,
-        #                             text_color=backgroundColor,
-                                    
-        #                             )
-        # self.import_button.grid(row=1, column=0,  pady=50, padx=10)
-
-        # self.sheetNameText = tk.Label(self, text="Current File:", font=("Arial", 8))
-        # self.sheetNameText.grid(row=2, column=0, pady=10, padx=10)
-
-        # self.sheetNameText = tk.Label(self, text="Current File:", font=("Arial", 8))
-        # self.sheetNameText.grid(row=3, column=0, pady=10, padx=10)
+        self.File_Preview.grid(row=16, column=0,  pady=10, padx=10, columnspan=16, rowspan=4)
 
 
 def main():
